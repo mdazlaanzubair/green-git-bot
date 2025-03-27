ac6b106dd5bfa7710861dc37b3b2a7d1e1c75432
Muhammad Azlaan Zubair <mdazlaan1996@gmail.com>
2025-03-28 04:16:13 +0500

    pushing to vercel

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ import router from "./routes/router.js"
 import swaggerDocument from "./swagger-output.json" with { type: "json" };
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+// adding base route
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.use(router);
 
@@ -18,3 +21,5 @@ app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+export default app;
